ceb9e39277acd95da615392763b43fab58817b39
lyqsbf <05yaqi.liu@gmail.com>
2026-02-12 15:49:53 +0100

    update

--- checker_bonus.h
+++ checker_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaqliu <yaqliu@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 14:42:40 by yaqliu            #+#    #+#             */
-/*   Updated: 2026/02/12 14:42:46 by yaqliu           ###   ########.fr       */
+/*   Updated: 2026/02/12 15:45:50 by yaqliu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -23,7 +23,7 @@ typedef struct s_stack
 	struct s_stack	*next;
 	int				value;
 	int				r_pos;
-}   t_stack;
+}	t_stack;
 
 int		ft_strcmp(char *str1, char *str2);
 void	ss(t_stack **a, t_stack **b);
